2606fd99ea3c02845d2620e29fefdc333b294546
Guiteau <55487060+Guiteau@users.noreply.github.com>
2019-12-31 12:57:52 +0000

    Update AppTest.java

--- Componentes/src/main/java/dad/javafx/components/AppTest.java
+++ Componentes/src/main/java/dad/javafx/components/AppTest.java
@@ -1,15 +1,18 @@
 package dad.javafx.components;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class AppTest extends Application{
-	
+public class AppTest extends Application {
+
 	DateChooser dateComponent;
 	VBox root;
 	HBox buttonHBox;
@@ -18,34 +21,52 @@ public class AppTest extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		dateComponent = new DateChooser();		
-		
+		dateComponent = new DateChooser();
+
 		startButton = new Button("Inicializar");
+		startButton.setOnAction(e -> onStartButtonAction(e));
+
 		queryButton = new Button("Consultar");
-		
+		queryButton.setOnAction(e -> onQueryButtonAction(e));
+
 		buttonHBox = new HBox(startButton, queryButton);
-		
+
 		buttonHBox.setSpacing(10);
 		buttonHBox.setAlignment(Pos.CENTER);
-		
-		root = new VBox(dateComponent, buttonHBox);
-		
+
+		root = new VBox(dateComponent.getView(), buttonHBox);
+
 		root.setSpacing(10);
 		root.setAlignment(Pos.CENTER);
-	
-		Scene scene = new Scene(root, 320, 200);
-		
+
+		Scene scene = new Scene(root, 420, 200);
+
 		primaryStage.setTitle("Date component tester");
 
 		primaryStage.setScene(scene);
 
 		primaryStage.show();
+
+	}
+
+	private void onQueryButtonAction(ActionEvent e) {
+		
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Fecha");
+		alert.setHeaderText(null);
+		alert.setContentText("La fecha seleccionada es: "+dateComponent.getDataDefined());
+		alert.showAndWait();
 		
 	}
 	
+	private void onStartButtonAction(ActionEvent e) {
+
+		dateComponent.setTodayData();
+
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-
 
 }
